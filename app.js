991231e832447ae5b29eeaab671ffb0e67e887d3
yuta-kestrel <yutahirooka8@gmail.com>
2026-07-15 19:29:27 +0900

    Update app.jsClear

--- app.js
+++ app.js
@@ -5,48 +5,8 @@ L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
-// テスト用グリッドライン
+drawGrid();
 
-// 横線
-L.polyline([
-    [36.395, 136.39],
-    [36.395, 136.43]
-], {
-    color: "#4da6ff",
-    weight: 2
-}).addTo(map);
+function drawGrid() {
 
-// 縦線
-L.polyline([
-    [36.37, 136.41],
-    [36.42, 136.41]
-], {
-    color: "#4da6ff",
-    weight: 2
-}).addTo(map);
-const interval = 30 / 3600; // 30秒 = 約0.008333度
-
-const bounds = map.getBounds();
-
-const south = bounds.getSouth();
-const north = bounds.getNorth();
-const west = bounds.getWest();
-const east = bounds.getEast();
-
-// 横線
-for (
-    let lat = Math.floor(south / interval) * interval;
-    lat <= north;
-    lat += interval
-) {
-    L.polyline(
-        [
-            [lat, west],
-            [lat, east]
-        ],
-        {
-            color: "#4da6ff",
-            weight: 1
-        }
-    ).addTo(map);
-        }
+}
