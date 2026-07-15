dcce0a234d1476701bc06cbb220569da9fb8807d
yuta-kestrel <yutahirooka8@gmail.com>
2026-07-15 17:22:52 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Project Kestrel Started");
+const map = L.map('map').setView([36.3948, 136.4076], 14);
+
+L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    attribution: '© 国土地理院',
+    maxZoom: 18
+}).addTo(map);
