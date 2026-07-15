19fba9ddf45f01322ffc5f9f7777d9f01ed7a590
yuta-kestrel <yutahirooka8@gmail.com>
2026-07-15 19:33:38 +0900

    Update app.js完全版

--- app.js
+++ app.js
@@ -1,12 +1,76 @@
 const map = L.map('map').setView([36.3948, 136.4076], 14);
 
+// 国土地理院
 L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
     attribution: '© 国土地理院',
     maxZoom: 18
 }).addTo(map);
 
-drawGrid();
+// グリッドを保存するレイヤー
+const gridLayer = L.layerGroup().addTo(map);
 
+// 30秒（0.008333333°）
+const interval = 30 / 3600;
+
+// グリッド描画
 function drawGrid() {
 
+    // 前回のグリッドを削除
+    gridLayer.clearLayers();
+
+    const bounds = map.getBounds();
+
+    const south = bounds.getSouth();
+    const north = bounds.getNorth();
+    const west = bounds.getWest();
+    const east = bounds.getEast();
+
+    // 横線
+    for (
+        let lat = Math.floor(south / interval) * interval;
+        lat <= north;
+        lat += interval
+    ) {
+
+        L.polyline(
+            [
+                [lat, west],
+                [lat, east]
+            ],
+            {
+                color: "#4da6ff",
+                weight: 1,
+                opacity: 0.8
+            }
+        ).addTo(gridLayer);
+
+    }
+
+    // 縦線
+    for (
+        let lon = Math.floor(west / interval) * interval;
+        lon <= east;
+        lon += interval
+    ) {
+
+        L.polyline(
+            [
+                [south, lon],
+                [north, lon]
+            ],
+            {
+                color: "#4da6ff",
+                weight: 1,
+                opacity: 0.8
+            }
+        ).addTo(gridLayer);
+
+    }
+
 }
+
+// 最初に描画
+drawGrid();
+
+// 地図を動かしたら描き直す
+map.on("moveend", drawGrid);
