8523884f6f600c84c4cf2aeab44f0a544b4efdd1
yuta-kestrel <yutahirooka8@gmail.com>
2026-07-15 18:30:39 +0900

    Update app.jsグリット追加

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
     attribution: '© 国土地理院',
     maxZoom: 18
 }).addTo(map);
+// テスト用グリッド
+const bounds = [
+    [36.385, 136.395],
+    [36.405, 136.415]
+];
+
+L.rectangle(bounds, {
+    color: "#4da6ff",
+    weight: 2,
+    fill: false
+}).addTo(map);
